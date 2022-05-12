6003c3f5c24c761479d15574c60572cfd76c56ba
Mateus Barbosa <62620931+FrostPrice@users.noreply.github.com>
2022-05-11 21:25:41 -0300

    Chore: Add more comments

--- FlappyBird/main.cpp
+++ FlappyBird/main.cpp
@@ -34,8 +34,9 @@ int main() {
     //FIM: COMANDOS PARA REPOSICIONAR O CURSOR NO INÍCIO DA TELA
     ///ALERTA: NãO MODIFICAR O TRECHO DE CÓDIGO, ACIMA.
 
-    srand(time(NULL));
+    srand(time(NULL)); // Obrigatório para conseguir gerar um valor aleatório a cada momento. E para poder usar o rand()
      
+    // Inicialização de variáveis
     int bird_x = 5, bird_y = 6, bird_y_old_position;
     int obstaculo_x = 110, obstaculo_y, obstaculo_x_old_position;
     int obstaculo2_x = obstaculo_x + 55, obstaculo2_y, obstaculo2_x_old_position;
@@ -43,22 +44,24 @@ int main() {
     int tecla; // Para conseguir checar qual tecla o usuário pressionou
     int placar = 0; // Highscore do usuário
     int placar_x = 50, placar_y = 22; // Localização do placar
-    int tamanho_passagem_1 = rand() % 15 + 3; // Gera valores aleatórios para a criação do local aonde o passáro deve atravessar
-    int tamanho_passagem_2 = rand() % 15 + 3; // Gera valores aleatórios para a criação do local aonde o passáro deve atravessar
+    int tamanho_passagem_1 = rand() % 20; // Gera valores aleatórios de 0 ao 19 para o obstaculo1
+    int tamanho_passagem_2 = rand() % 20; // Gera valores aleatórios de 0 ao 19 para o obstaculo2
     float velocidade = 150; // Velocidade em que o jogo esta no momento
 
-    bool playing = false; // Determina se o jogo deve começar ou não
+    bool playing = false; // Determina se o jogo está rodando ou não
 
-    system("cls"); // Para rodar o jogo no terminal do VSCode com o layout sem ter artefatos, ou em qualquer terminal
+    system("cls"); // Usado para limpar o terminal, evitando assim qualquer distorção no layout
 
+    // Desenho do cenário
     cout<<"-----------------------------------------------------------------------------------------------------";
     cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
     cout<<"-----------------------------------------------------------------------------------------------------";
 
     // Menu inicial
-    coord.X = 40; coord.Y = 10;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-    cout << "Bem-vindo ao FlappyBird!";
+    coord.X = 40; coord.Y = 10; // Seta uma coordenada X e Y
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord); // Seleciona o ponto da coordenada informada. Com o ponto de origem sendo o canto superior esquerdo
+    cout << "Bem-vindo ao FlappyBird!"; // Coloca um item na cordenada selecionada
+    // (Passos Anteriores se repetem para todos os items que envolvem coordenadas)
     coord.X = 40; coord.Y = 11;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
     cout << "Aperte W para comecar a jogar";
@@ -66,8 +69,9 @@ int main() {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
     cout << "Q (Sair do Jogo)";
 
-    tecla = getch(); // Valida a tecla apertada no menu inicial
+    tecla = getch(); // Espera por um input do usuário
 
+    // Valida a tecla (Maiúscula ou Minúscula) apertada no menu inicial
     switch(tecla) {
         case 'w': case 'W':
             system("cls");
@@ -83,8 +87,7 @@ int main() {
             // Lógica para fazer o jogo funcionar
             do {
                 // Poscicionamento do placar
-                coord.X = placar_x;
-                coord.Y = placar_y;
+                coord.X = placar_x; coord.Y = placar_y;
                 SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
                 cout << "PLACAR: " << placar;
                 
@@ -99,8 +102,6 @@ int main() {
                 SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
                 cout<<char(190);
 
-                bird_y_old_position = bird_y;
-
                 // Posiciona o obstáculo
                 obstaculo_y=1;
                 obstaculo2_y=1;
@@ -110,19 +111,25 @@ int main() {
                     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
                     cout<<" ";
 
+                    // Remove o rastro do obstáculo2
                     coord.X = obstaculo2_x_old_position;    coord.Y = obstaculo2_y;
                     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
                     cout<<" ";
 
+                    // Define a posição e tamanho da passagem para o pássaro
                     coord.X = obstaculo_x;    coord.Y = obstaculo_y;
                     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+                    
+                    // Define a parte do obstáculo que o pássaro não pode passar
                     if(obstaculo_y < tamanho_passagem_1 - 1 || obstaculo_y > tamanho_passagem_1 + 1) {
                         cout<<char(219);
                     } else {
+                        // Define a passagem do obstáculo
                         cout<<" ";
                     }
                     obstaculo_y++;
 
+                    // Só mostra o obstáculo2 após ele chegar a parte inicial da tela, para evitar bugs no layout e ter consistencia no espaço entre os obstáculos
                     if(obstaculo2_x < 110) {
                         // Posiciona Obstáculo 2
                         coord.X = obstaculo2_x;    coord.Y = obstaculo2_y;
@@ -136,16 +143,11 @@ int main() {
                     obstaculo2_y++;
                 }
 
-                // Verifica a colisão com obstaculo 1
+                // Valida se a coordenada X do passáro é a mesma da coordanada X do obstáculo1
+                // E se a coordenada Y do pássaro é diferente da coordenada da passagem do obstáculo
                 if(bird_x == obstaculo_x && (bird_y < tamanho_passagem_1 - 1 || bird_y > tamanho_passagem_1 + 1)) {
-                    coord.X = 50;    coord.Y = 10;
-                    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-                    cout << "Voce Perdeu!";
                     playing = false;
                 } else if(bird_x == obstaculo2_x && (bird_y < tamanho_passagem_2 - 1 || bird_y > tamanho_passagem_2 + 1)) { // Verifica a colisão com obstaculo 2
-                    coord.X = 50;    coord.Y = 10;
-                    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-                    cout << "Voce Perdeu!";
                     playing = false;
                 }
                 
@@ -157,6 +159,7 @@ int main() {
                 if (tecla == 'w'){
                     // Pássaro sobe 1 posição ao precionar a tecla W
                     bird_y--;
+                    // Reseta a tecla para que ela não fique "precionada"
                     tecla='0';
                 } else {
                     // Pássaro cai 1 posição se não for pressionado para subir
@@ -168,6 +171,8 @@ int main() {
                     playing = false;
                 }
 
+                // Armazena a última posição do pássaro
+                bird_y_old_position = bird_y;
 
                 // Armazena a última posição no eixo X do obstáculo1 e obstáculo2
                 obstaculo_x_old_position = obstaculo_x;
@@ -175,29 +180,29 @@ int main() {
 
                 //  Obstáculo1 avança uma posição para esquerda
                 obstaculo_x--;
-                //  Obstáculo2 avança uma posição para esquerda
+                // Mesma coisa ocorre para o obstáculo2
                 obstaculo2_x--;
 
-                // Tempo de espera
+                // Velocidade atual do jogo
                 Sleep(velocidade);
                 
                 // Adiciona pontuação ao placar quando passar por um obstáculo  
                 if (bird_x == obstaculo_x + 1 or bird_x == obstaculo2_x + 1) {
                     placar++; // Adiciona +1 ao placar
-                    // Se o valor da pontuação for diferente de zero e ser multiplo de cinco
                     if (placar != 0 && placar % 5 == 0) {
+                    // Se o valor da pontuação for diferente de zero e ser multiplo de cinco
                     // a velocidade aumenta a cada cinco obstaculos passados
-                    velocidade = velocidade * 0.75; // A quantidade que a velocidade aumenta é de 5%
+                    velocidade = velocidade * 0.75; // A quantidade que a velocidade aumenta é de 25%
                     }
                 }
             
-                // Retona obstáculo para posição inicial e randomiza a passagem para o pássaro
+                // Retona obstáculo para a posição inicial e randomiza a passagem para o pássaro
                 if(obstaculo_x < 0) {
                     obstaculo_x = 110;
-                    tamanho_passagem_1 = rand() % 16 + 3;
+                    tamanho_passagem_1 = rand() % 20;
                 } else if(obstaculo2_x < 0) {
                     obstaculo2_x = 110;
-                    tamanho_passagem_2 = rand() % 16 + 3;
+                    tamanho_passagem_2 = rand() % 20;
                 }
 
                 if(!playing) {
@@ -210,12 +215,12 @@ int main() {
                     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
                     cout << "Deseja jogar novamente? (S) ou (N)";
 
-                    tecla = getch();
+                    tecla = getch(); // Espera por um input do usuário
                     switch(tecla) {
-                        // Reseta o jogo. Ao setar os valores das variáveis ao seu estado inicial
                         case 's': case 'S':
-                            system("cls");
+                        // Reseta o jogo. Declarando os valores das variáveis ao seu estado inicial
                             tecla = '0';
+                            system("cls");
 
                             cout<<"-----------------------------------------------------------------------------------------------------";
                             cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
@@ -224,17 +229,17 @@ int main() {
                             bird_y = 6;
 
                             obstaculo_x = 110;
-                            tamanho_passagem_1 = rand() % 15 + 3;
+                            tamanho_passagem_1 = rand() % 20;
 
                             obstaculo2_x = obstaculo_x + 30;
-                            tamanho_passagem_2 = rand() % 15 + 3;
+                            tamanho_passagem_2 = rand() % 20;
 
                             placar = 0;
 
                             playing = true;
                             break;
-                        // Sai do jogo
                         case 'n': case 'N':
+                        // Sai do jogo
                             system("cls");
                             system("exit");
                             break;
@@ -242,6 +247,7 @@ int main() {
                 }
             } while (playing);
             break;
+        // Sai do jogo
         case 'q': case 'Q':
             system("cls");
             system("exit");
