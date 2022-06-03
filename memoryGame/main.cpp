c0aceff0a13c04dcc1595082acd3679640fab0cd
FrostPrice <barbosamat16@gmail.com>
2022-06-02 23:57:05 -0300

    Feat: Add new project memoryGame

---
+++ memoryGame/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    /*
+        DICAS:
+        Usar o srand() e rand() % limite, para sortear de 1 à 4, para definir o que será feito para a matriz gabarito
+        Definir valores fixos para a matiz gabarito
+        Matriz jogo pode começar zerada
+        As escolhas são feitas atraves das posições da matriz (Ex: [1][3])
+        ssytem("cls")
+        Texto na tela
+        system("pause") 
+    */
+
+    return 0;
+}
